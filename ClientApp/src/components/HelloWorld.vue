21bde732831e83b9966523c23779fd30ebe8a16d
Chamyys <egorchaly@gmail.com>
2023-07-05 16:34:38 +0300

    5 july

--- ClientApp/src/components/HelloWorld.vue
+++ ClientApp/src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ const count = ref(0)
   <h1 class="text-3xl font-bold">{{ msg }}</h1>
 
   <p>
-    Recommended IDE setup00000:
+    Recommended IDE setup:
     <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
     +
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
@@ -26,7 +26,7 @@ const count = ref(0)
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count * 10 }}</button>
+  <button type="button" @click="count++">count is: {{ count }}</button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
@@ -35,6 +35,6 @@ const count = ref(0)
 
 <style scoped>
 a {
-  color: #42b983;
+  color: #40b983;
 }
 </style>
